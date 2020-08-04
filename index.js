b5066d270fdb25ccce89bbad8a351bdeb2d334e2
Mag1cc <matisoch04@gmail.com>
2020-08-04 16:57:01 +0100

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: 'NzE3MTA0NjczMTQ3OTc3NzI4.XtVhUg.3GgApcXMeEQQcyLK0AgOVm7STNc' });
-
-manager.spawn();
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
